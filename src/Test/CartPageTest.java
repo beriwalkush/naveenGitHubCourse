cb4a3efd16948bf5fb3a728d7e1bbaf31711ed28
Kbcoder5 <kush.beriwal@gmail.com>
2022-06-16 07:51:03 +0530

    cartPage changes

--- src/Test/CartPageTest.java
+++ src/Test/CartPageTest.java
@@ -3,6 +3,12 @@ package Test;
 public class CartPageTest {
 
 	public void test3(){
-		System.out.println("CartPage Test Start");
+		System.out.println("CartPage Test3 Start");
 	}
+	
+	public void test4(){
+		System.out.println("CartPage Test4 Start");
+	}
+	
+	
 }
