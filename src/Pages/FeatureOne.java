70127d8be673dfa4fbef449a699daaa8d656a657
Kbcoder5 <kush.beriwal@gmail.com>
2022-06-16 23:09:01 +0530

    Feature Class with Exp 2

--- src/Pages/FeatureOne.java
+++ src/Pages/FeatureOne.java
@@ -5,6 +5,7 @@ public class FeatureOne {
 	public static void main(String[] args) {
 		
 		System.out.println("Feature One Tag Example");
+		System.out.print("Feature One Tag Example Two");
 	}
 
 }
