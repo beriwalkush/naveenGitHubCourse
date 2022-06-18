2557e53c309f68426ae00c8feb22cdade6c152cc
beriwalkush <107637580+beriwalkush@users.noreply.github.com>
2022-06-18 22:24:32 +0530

    FeatureOne.java
    Code added by Developer 2

---
+++ src/Pages/FeatureOne.java
@@ -0,0 +1,12 @@
+package Pages;
+
+public class FeatureOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Feature One Tag Example Dev 2");
+		System.out.print("Feature One Tag Example Two Dev 2");
+		System.out.print("Feature One Tag Example Three Dev 2");
+	}
+
+}
